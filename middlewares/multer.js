9d4dded7d582ec744e4a022e97a91b0e333d8425
adharshkc <adharshkc2017@gmail.com>
2024-01-24 10:35:32 +0530

    removed images

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,19 +1,35 @@
-const multer = require("multer")
-const path = require('path')
-const upload = function(){
+const multer = require("multer");
+const path = require("path");
+const upload = function () {
+  const storage = multer.diskStorage({
+    destination: "./public/image",
+    filename: (req, file, cb) => {
+      cb(
+        null,
+        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+      );
+    },
+  });
+  const fileFilter = (req, file, cb) => {
+    const allowedMineType = ["image/jpg", "image/png", "image/jpg"];
+    if (allowedMineType.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(
+        new Error(
+          "Invalid file type. Only JPEG, PNG, and GIF files are allowed."
+        ),
+        false
+      );
+    }
+  };
 
-    const storage = multer.diskStorage({
-        destination: "./public/image",
-        filename: (req, file, cb) => {
-            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-          }
-    })
-    
-    const upload = multer({
-        storage: storage,
-        limits: {fileSize: 5000000}
-    }).single('image')
-    return upload;
-}
+  const upload = multer({
+    storage: storage,
+    limits: { fileSize: 5000000 },
+    fileFilter: fileFilter,
+  }).single("image");
+  return upload;
+};
 
-module.exports = {upload};
+module.exports = { upload };
